05e5bd59805b06391e4a9035a697bfed6f091ca8
Trevor Sands <trevor@bitshift.software>
2025-11-15 22:00:08 -0500

    Add utilities for order ID tracking

--- kraken/utilities.py
+++ kraken/utilities.py
@@ -1,5 +1,7 @@
+import re
 from datetime import datetime, timezone
 from time import time
+from uuid import uuid4
 
 
 def bool_from_str(value: str | bool) -> bool:
@@ -19,3 +21,17 @@ def get_nonce() -> int:
 
 def utc_now() -> datetime:
     return datetime.now(timezone.utc)
+
+
+def rand_uuid() -> str:
+    return str(uuid4())
+
+
+def is_uuid(i: str) -> bool:
+    pattern = r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+    return bool(re.match(pattern, i, re.IGNORECASE))
+
+
+def is_txid(i: str) -> bool:
+    pattern = r"^[A-Z0-9]{6}-[A-Z0-9]{5}-[A-Z0-9]{6}$"
+    return bool(re.match(pattern, i, re.IGNORECASE))
